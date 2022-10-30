54a3e2810d87f217c0e409c829f0873e6bc46f06
Adán G. Medrano-Chávez <ryuuba@gmail.com>
2022-10-30 14:51:56 -0600

    Export tictoc works fine

---
+++ out/gcc-release/tictoc18_m.o.d
@@ -0,0 +1,4 @@
+out/gcc-release//tictoc18_m.o: tictoc18_m.cc tictoc18_m.h \
+ /home/geo/omnetpp-6.0.1/include/omnetpp.h
+tictoc18_m.h:
+/home/geo/omnetpp-6.0.1/include/omnetpp.h:
---
+++ out/gcc-release/txc18.o.d
@@ -0,0 +1,4 @@
+out/gcc-release//txc18.o: txc18.cc \
+ /home/geo/omnetpp-6.0.1/include/omnetpp.h tictoc18_m.h
+/home/geo/omnetpp-6.0.1/include/omnetpp.h:
+tictoc18_m.h:
